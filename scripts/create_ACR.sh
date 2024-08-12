9aa7a95782b7aea5864bd5f14f243d714f41ad68
RolaMmss <rola.el-moallem@hotmail.fr>
2024-08-12 17:33:25 +0200

    feat:

--- scripts/create_ACR.sh
+++ scripts/create_ACR.sh
@@ -6,29 +6,29 @@ set +o allexport
 # Login to Azure
 az login
 
-az container create \
-            --resource-group $RESSOURCE_GROUP \
-            --name ContainerInstanceApi --image $DOCKERHUB_USERNAME/repo_docker:latest --cpu 1 \
-            --memory 1 \
-            --ip-address public \
-            --ports 80 8000 \
-            --environment-variables \
-              "SUBSCRIPTION_ID"=$SUBSCRIPTION_ID \
-              "RESSOURCE_GROUP"=$RESSOURCE_GROUP \
-              "WORKSPACE_NAME"=$WORKSPACE_NAME \
-              "SERVER"=$SERVER \
-              "DATABASE"=$DATABASE \
-              "POSTGRES_USER"=$POSTGRES_USER \
-              "PASSWORD"=$PASSWORD \
-              "SECRET_KEY"=$SECRET_KEY
-
 # az container create \
-#             --resource-group RG_Rola_certif2024 \
-#             --name ContainerInstanceApi \    
-#             --image rola123/repo_docker:latest \    
-#             --cpu 1 \
+#             --resource-group $RESSOURCE_GROUP \
+#             --name ContainerInstanceApi --image $DOCKERHUB_USERNAME/repo_docker:latest --cpu 1 \
 #             --memory 1 \
 #             --ip-address public \
 #             --ports 80 8000 \
-#              --registry-username rola123 \
-#             --registry-password Do-302868
+#             --environment-variables \
+#               "SUBSCRIPTION_ID"=$SUBSCRIPTION_ID \
+#               "RESSOURCE_GROUP"=$RESSOURCE_GROUP \
+#               "WORKSPACE_NAME"=$WORKSPACE_NAME \
+#               "SERVER"=$SERVER \
+#               "DATABASE"=$DATABASE \
+#               "POSTGRES_USER"=$POSTGRES_USER \
+#               "PASSWORD"=$PASSWORD \
+#               "SECRET_KEY"=$SECRET_KEY
+
+az container create \
+    --resource-group $RESSOURCE_GROUP \
+    --name container-instance-api \
+    --image $DOCKERHUB_USERNAME/repo_docker:latest \
+    --cpu 1 \
+    --memory 1 \
+    --ip-address public \
+    --ports 80 8000 \
+    --registry-username $DOCKERHUB_USERNAME \
+    --registry-password $DOCKERHUB_PASSWORD
