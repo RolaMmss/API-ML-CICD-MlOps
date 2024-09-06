a31caaa2721a4eded936326e12ed9d3a1b2ecd16
RolaMmss <rola.el-moallem@hotmail.fr>
2024-09-06 16:22:07 +0200

    mod

--- scripts/create_ACR.sh
+++ scripts/create_ACR.sh
@@ -6,25 +6,9 @@ set +o allexport
 # Login to Azure
 az login
 
-# az container create \
-#             --resource-group $RESSOURCE_GROUP \
-#             --name ContainerInstanceApi --image $DOCKERHUB_USERNAME/repo_docker:latest --cpu 1 \
-#             --memory 1 \
-#             --ip-address public \
-#             --ports 80 8000 \
-#             --environment-variables \
-#               "SUBSCRIPTION_ID"=$SUBSCRIPTION_ID \
-#               "RESSOURCE_GROUP"=$RESSOURCE_GROUP \
-#               "WORKSPACE_NAME"=$WORKSPACE_NAME \
-#               "SERVER"=$SERVER \
-#               "DATABASE"=$DATABASE \
-#               "POSTGRES_USER"=$POSTGRES_USER \
-#               "PASSWORD"=$PASSWORD \
-#               "SECRET_KEY"=$SECRET_KEY
-
 az container create \
     --resource-group $RESSOURCE_GROUP \
-    --name container-instance-api \
+    --name containerinstanceapi \
     --image $DOCKERHUB_USERNAME/repo_docker:latest \
     --cpu 1 \
     --memory 1 \
